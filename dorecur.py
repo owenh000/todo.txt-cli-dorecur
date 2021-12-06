93607107471c1aa83fbd357208baec2adecc8caa
Ferenc Erki <erkiferenc@gmail.com>
2021-12-06 23:32:50 +0100

    Add test for tasks with dates to preserve

--- dorecur.py
+++ dorecur.py
@@ -178,6 +178,10 @@ def make_new_task(line, now=datetime.date.today()):
     'Test task rec:3d'
     >>> make_new_task('(A) 1970-01-01 Test task rec:3d', now)
     '(A) Test task rec:3d'
+    >>> make_new_task('(A) 1970 Test task rec:3d', now)
+    '(A) 1970 Test task rec:3d'
+    >>> make_new_task('(A) Test task with 1970-01-01 in it rec:3d', now)
+    '(A) Test task with 1970-01-01 in it rec:3d'
     >>> make_new_task('Test task rec:+3d', now)
     'Test task rec:+3d'
     >>> make_new_task('Test task t:1970-01-01 rec:3d', now)
